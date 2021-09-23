ce43f4aa8bddddbed282b31f72aa0d2450a78fca
Ashik Chapagain <projectashik@gmail.com>
2021-09-23 10:31:27 +0545

    Icon Updated

--- components/Corona/TopCountries.tsx
+++ components/Corona/TopCountries.tsx
@@ -1,9 +1,9 @@
-import { Card } from "./Card";
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { CountryType } from "../../shared/types/CountryType";
-import { useState } from "react";
+import { Card } from './Card';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { CountryType } from '../../shared/types/CountryType';
+import { useState } from 'react';
 
 export const TopCountries = ({ data }: any) => {
   const [initialValue, setInitialValue] = useState(0);
@@ -24,44 +24,44 @@ export const TopCountries = ({ data }: any) => {
   };
   return (
     <Card>
-      <div className="flex justify-between items-center">
+      <div className='flex justify-between items-center'>
         <strong>Top Countries</strong>
-        <div className="flex items-center">
+        <div className='flex items-center'>
           <button
-            className="border py-1 px-2 focus:border-gray-900 disabled:opacity-30"
+            className='border py-1 px-2 focus:border-gray-900 disabled:opacity-30'
             disabled={initialValue <= 0}
             onClick={() => changeLeft()}
           >
-            <FontAwesomeIcon icon={faAngleLeft} />
+            <FontAwesomeIcon className='w-4 h-4' icon={faAngleLeft} />
           </button>
           <button
-            className="border py-1 px-2 focus:border-gray-900 disabled:opacity-30"
+            className='border py-1 px-2 focus:border-gray-900 disabled:opacity-30'
             onClick={() => changeRight()}
             disabled={finalValue >= data.length}
           >
-            <FontAwesomeIcon icon={faAngleRight} />
+            <FontAwesomeIcon className='w-4 h-4' icon={faAngleRight} />
           </button>
         </div>
       </div>
-      <div className="flex flex-col gap-8 mt-4">
+      <div className='flex flex-col gap-8 mt-4'>
         {data.slice(initialValue, finalValue).map((country: CountryType) => {
           return (
             <div
               key={country.countryInfo._id}
-              className="flex items-center justify-between"
+              className='flex items-center justify-between'
             >
-              <div className="flex items-center">
+              <div className='flex items-center'>
                 <Image
                   src={country.countryInfo.flag}
                   alt={country.country}
-                  width="50px"
-                  height="30px"
-                  className="rounded shadow-xl"
+                  width='50px'
+                  height='30px'
+                  className='rounded shadow-xl'
                 />
-                <span className="ml-3 font-bold">{country.country}</span>
+                <span className='ml-3 font-bold'>{country.country}</span>
               </div>
-              <div className="flex items-center">
-                <span className="text-red-500 font-bold">{country.cases}</span>
+              <div className='flex items-center'>
+                <span className='text-red-500 font-bold'>{country.cases}</span>
               </div>
             </div>
           );
