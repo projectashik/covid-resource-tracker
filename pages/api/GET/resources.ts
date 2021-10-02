003a36976711c36b195beae16406cdd6f230d158
Ashik Chapagain <projectashik@gmail.com>
2021-10-02 21:16:03 +0545

    Added some margins in risk modal

--- pages/api/GET/resources.ts
+++ pages/api/GET/resources.ts
@@ -1,21 +1,23 @@
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextApiResponse, NextApiRequest } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<any> {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<any> {
   const { type } = req.query;
   const request = await fetch(<any>process.env.DB_URL, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Basic cHJvamVjdGFzaGlrOlRmYndqcT9AMzY5`
+      'Content-Type': 'application/json',
+      Authorization: `Basic cHJvamVjdGFzaGlrOlRmYndqcT9AMzY5`,
     },
     body: JSON.stringify({
-      operation: "sql",
-      sql: `SELECT * FROM resources.resources ORDER BY __createdtime__ DESC`
-    })
-  })
-
+      operation: 'sql',
+      sql: `SELECT * FROM resources.resources ORDER BY __createdtime__ DESC`,
+    }),
+  });
 
   const data = await request.json();
 
-  res.status(200).json(data)
+  res.status(200).json(data);
 }
